1cc69132f998c0b21e54f61ef1531694fcd5ad9a
moses ngige <moses32ngigz@gmail.com>
2022-08-22 11:37:22 +0300

    commit tree 4279afb81a9c01107fcec856ef8f9f80c5022fc0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
 
+class Student < User
+    attr_accessor :knowledge,:string
+    def initialize 
+        @knowledge = []
+    end
+    def learn(string)
+        @string = string
+        @knowledge<<string
+    end
+    def knowledge
+        @knowledge
+    end
 end
